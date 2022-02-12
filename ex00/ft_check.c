bed2295e33dc30a98185e3ff6a1dc1196271b913
Orson DESSEIN <odessein@e3r2p2.clusters.42paris.fr>
2022-02-12 21:57:17 +0100

    obvious cases done ! : )

--- ex00/ft_check.c
+++ ex00/ft_check.c
@@ -20,7 +20,8 @@ int	check_colup(int tab[4][4], int *a) //a pointe sur la val de la case
 		}
 		i++;
 		if (tab[0][0] = count) 
-			return(1); // ex: si a[i] = 4, count = 1, on regarde si ca fit avec l'argument donne, si oui, placement valide, retour(1)
+			return(1); // ex: si a[i] = 4, count = 1, on regarde si ca fit avec 
+						//l'argument donne, si oui, placement valide, retour(1)
 	}
 	return (0); //sinon, une fois toute la column parcourue, placement non valide, retour (0)
 }	
--- ex00/ft_rush.c
+++ ex00/ft_rush.c
@@ -6,7 +6,7 @@
 /*   By: odessein <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 15:41:17 by odessein          #+#    #+#             */
-/*   Updated: 2022/02/12 20:40:03 by odessein         ###   ########.fr       */
+/*   Updated: 2022/02/12 21:56:28 by odessein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 void	ft_ultimate_obvious(int skyscraper[4][4], int browser)
@@ -35,15 +35,51 @@ void	ft_ultimate_obvious(int skyscraper[4][4], int browser)
 	}
 }
 
+void	ft_ultimate_obvious1(int skyscraper[4][4], int browser)
+{
+	if (browser < 4)
+		skyscraper[0][browser] = 4;
+	if (browser > 7 && browser < 12)
+		skyscraper[browser % 4][0] = 4;
+	if (browser < 8 && browser > 3)
+		skyscraper[3][browser % 4] = 4;
+	if (browser > 11)
+		skyscraper[browser % 4][3] = 4;
+}
+
+void	ft_ultimate_obvious2(int skyscraper[4][4], int browser)
+{
+	if (browser < 4)
+		skyscraper[2][browser] = 4;
+	if (browser > 7 && browser < 12)
+		skyscraper[browser % 4][2] = 4;
+	if (browser < 8 && browser > 3)
+		skyscraper[1][browser % 4] = 4;
+	if (browser > 11)
+		skyscraper[browser % 4][1] = 4;
+}
+
 void	ft_obvious(int skyscraper[4][4], int input[16])
 {
-	int	browser;
+	int	brows;
 
-	browser = 0;
-	while (browser < 16)
+	brows = 0;
+	while (brows < 16)
 	{
-		if (input[browser] == 4)
-			ft_ultimate_obvious(skyscraper, browser);
-		browser++;
+		if (input[brows] == 1)
+			ft_ultimate_obvious1(skyscraper, brows);
+		if (input[brows] == 4)
+			ft_ultimate_obvious(skyscraper, brows);
+		if (input[brows] == 3)
+		{
+			if (brows < 4 || (brows > 7 && brows < 12))
+			{
+				if (input[brows + 4] == 2)
+					ft_ultimate_obvious2(skyscraper, brows);
+			}
+			else if (input[brows - 4] == 2)
+				ft_ultimate_obvious2(skyscraper, brows);
+		}
+		brows++;
 	}	
 }
