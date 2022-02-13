c12c6454bb66a19b9d9624c52be1cc45f8149bad
Kelly HUYNH <khuynh@e3r3p3.clusters.42paris.fr>
2022-02-13 13:06:29 +0100

    ouuuuuui

--- ex00/ft_finalcheck.c
+++ ex00/ft_finalcheck.c
@@ -6,9 +6,14 @@
 /*   By: khuynh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 11:49:56 by khuynh            #+#    #+#             */
-/*   Updated: 2022/02/13 12:00:56 by khuynh           ###   ########.fr       */
+/*   Updated: 2022/02/13 13:04:32 by khuynh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	ft_chk_raw_l_val(int skyscraper[4][4], int line, int input[16]);
+int	ft_chk_raw_r_val(int skyscraper[4][4], int line, int input[16]);
+int	ft_chk_col_u_val(int skyscraper[4][4], int col, int input[16]);
+int	ft_chk_col_d_val(int skyscraper[4][4], int col, int input[16]);
 
 int	check_dbl(int nb, int skyscrap[4][4], int col, int row)
 {
@@ -30,4 +35,19 @@ int	check_dbl(int nb, int skyscrap[4][4], int col, int row)
 		j++;
 	}
 	return (1);
+}
+
+int	ft_ultimate_check(int tab[4][4], int line, int col, int input[16])
+{
+	if (ft_chk_raw_l_val(int skyscraper[4][4], int line, int input[16]));
+		return (1);
+	if (ft_chk_raw_r_val(int skyscraper[4][4], int line, int input[16]));
+		return (1);
+	if (ft_chk_col_u_val(int skyscraper[4][4], int col, int input[16]));
+		return (1);
+	if (ft_chk_col_d_val(int skyscraper[4][4], int col, int input[16]));
+		return (1);
+	if (check_dbl(int nb, int skyscrap[4][4], int col, int row));
+		return (1);
+	return (0);
 }
